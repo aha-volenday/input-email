ea0a8184433beae43d06a8dc802693bdcbba7110
Jerome Miranda <jerome.m.miranda@gmail.com>
2019-09-17 12:40:18 +0800

    v0.1.6

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
-import InputDate from '@volenday/input-date';
 import validate from 'validate.js';
-import { Button, Form, Input, Popover } from 'antd';
+import { Form, Input } from 'antd';
 
 import './styles.css';
 
 export default class InputEmail extends Component {
-	state = { errors: [], hasChange: false, isPopoverVisible: false };
+	state = { errors: [] };
 
 	onChange = async value => {
-		const { action, id, onChange, onValidate } = this.props;
+		const { id, onChange, onValidate } = this.props;
 
 		onChange(id, value);
 		const errors = this.validate(value);
-		await this.setState({ errors, localValue: value, hasChange: action === 'add' ? false : true });
+		await this.setState({ errors, localValue: value });
 		if (onValidate) onValidate(id, errors);
 	};
 
@@ -29,10 +28,6 @@ export default class InputEmail extends Component {
 
 		const errors = validate({ [id]: value }, constraints);
 		return validate.isEmpty(value) && !required ? [] : errors ? errors[id] : [];
-	};
-
-	handlePopoverVisible = visible => {
-		this.setState({ isPopoverVisible: visible });
 	};
 
 	renderInput() {
@@ -64,43 +59,9 @@ export default class InputEmail extends Component {
 		);
 	}
 
-	renderPopover = () => {
-		const { isPopoverVisible } = this.state;
-		const { id, label = '', historyTrackValue = '', onHistoryTrackChange } = this.props;
-
-		return (
-			<Popover
-				content={
-					<InputDate
-						id={id}
-						label={label}
-						required={true}
-						withTime={true}
-						withLabel={true}
-						value={historyTrackValue}
-						onChange={onHistoryTrackChange}
-					/>
-				}
-				trigger="click"
-				title="History Track"
-				visible={isPopoverVisible}
-				onVisibleChange={this.handlePopoverVisible}>
-				<span class="float-right">
-					<Button
-						type="link"
-						shape="circle-outline"
-						icon="warning"
-						size="small"
-						style={{ color: '#ffc107' }}
-					/>
-				</span>
-			</Popover>
-		);
-	};
-
 	render() {
-		const { errors, hasChange } = this.state;
-		const { action, label = '', required = false, withLabel = false, historyTrack = false } = this.props;
+		const { errors } = this.state;
+		const { label = '', required = false, withLabel = false } = this.props;
 
 		const formItemCommonProps = {
 			colon: false,
@@ -110,11 +71,6 @@ export default class InputEmail extends Component {
 			validateStatus: errors.length != 0 ? 'error' : 'success'
 		};
 
-		return (
-			<Form.Item {...formItemCommonProps}>
-				{historyTrack && hasChange && action !== 'add' && this.renderPopover()}
-				{this.renderInput()}
-			</Form.Item>
-		);
+		return <Form.Item {...formItemCommonProps}>{this.renderInput()}</Form.Item>;
 	}
 }
